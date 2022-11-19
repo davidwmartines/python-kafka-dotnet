2e07cfeac541ceb92df03b5b87cead267e53a647
davidwmartines <d5172@yahoo.com>
2022-11-19 07:46:27 -0600

    Clean up program.

--- dotnet-consumer/Program.cs
+++ dotnet-consumer/Program.cs
@@ -1,9 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("dotnet consumer starting up");
-var counter = 0;
-var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
-while (max == -1 || counter < max)
+﻿using System;
+using System.Threading.Tasks;
+using Events;
+
+
+namespace Example.Consumer
 {
-    Console.WriteLine($"Counter: {++counter}");
-    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("dotnet consumer starting up");
+
+            Console.WriteLine(Environment.GetEnvironmentVariable("SCHEMA_REGISTRY_URL"));
+
+            Console.WriteLine(Environment.GetEnvironmentVariable("BOOTSTRAP_SERVERS"));
+
+
+            var e = new PullRequestOpened();
+
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+            }
+
+
+        }
+
+    }
 }
