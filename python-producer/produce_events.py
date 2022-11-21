493eae57f9a79d51ea3a67f53f824558c93675dd
davidwmartines <d5172@yahoo.com>
2022-11-20 20:17:49 -0600

    produce cloudevent.

--- python-producer/produce_events.py
+++ python-producer/produce_events.py
@@ -10,6 +10,10 @@ from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.serialization import MessageField, SerializationContext
 
+from cloudevents.kafka import to_binary
+from cloudevents.http import CloudEvent
+
+
 env = envparse.Env()
 env.read_envfile()
 
@@ -34,17 +38,30 @@ def main(*argv):
     while True:
         id += 1
         logger.info(f"opening pull request {id}")
-        pr = {
+        pullrequest_data = {
             "id": id,
             "url": f"https://github.com/bartsimpson/repo/pulls/{id}",
             "author": "Bart Simpson",
             "title": "test pr",
             "opened_on": datetime.utcnow(),
         }
+        event = CloudEvent.create(
+            {
+                "type": "pullrequest_created",
+                "source": "python-producer",
+                "partitionkey": str(id),
+                "content-type": "application/avro",
+            },
+            data=pullrequest_data,
+        )
+        message = to_binary(
+            event,
+            data_marshaller=lambda e: avro_serializer(
+                e, SerializationContext(topic, MessageField.VALUE)
+            ),
+        )
         producer.produce(
-            topic=topic,
-            key=str(id),
-            value=avro_serializer(pr, SerializationContext(topic, MessageField.VALUE)),
+            topic=topic, key=message.key, headers=message.headers, value=message.value
         )
         sleep(2)
 
